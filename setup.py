02655c1e0874733c6f7b2cb520c60f30237b8215
Eli Flesher <eli@eflee.us>
2015-05-20 22:12:53 -0700

    Updating setup.py with classifiers

--- setup.py
+++ setup.py
@@ -6,17 +6,26 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     
+    include_package_data = True,
     install_requires=['mock>=1.0.1', 'py2-ipaddress>=2.0', 'requests>=2.5.0', 'zope.interface>=4.1.1'],
     tests_require=['requests-mock>=0.5.1'],
-
-    include_package_data = True,
+    test_suite='test',
 
     # metadata for upload to PyPI
     author="Eli Flesher",
     author_email="eli@eflee.us",
     description="A generic library for getting your externally visible IP address in python.",
     license="Apache",
-    keywords="ip network",
-    url="https://github.com/eflee/pyechoip",  # project home page, if any
-    test_suite='test',
+    url="https://github.com/eflee/pyechoip",
+    keywords=["ip", "network", "api"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Internet",
+        "Topic :: Other/Nonlisted Topic",
+        ],
 )
